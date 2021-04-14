d11a964f763ba859c4c9ac0d26da7110fe139575
EconMike <datamike8@gmail.com>
2021-04-14 18:09:04 -0400

    Update CPI_one_chart_main.R

--- CPI_one_chart_main.R
+++ CPI_one_chart_main.R
@@ -34,16 +34,20 @@ ggplot(df4,aes(x=date, y=value, group=Prices))+
   scale_x_date(date_breaks = "5 months",limits=c(as.Date("2019-01-01"),as.Date("2021-03-01")),labels = date_format("%b/%Y"),expand = c(0, 0))+
   scale_y_continuous(limits = c(0, 4), breaks = seq(0, 4, by = 1),expand = c(0, 0),labels = function(value) paste0(value, "%"))+
   labs(title = "The Economic Monitor: Inflation", 
-       caption = "*CORE Consumer Price Index excluding food and energy commodities\nThe CPI displayed is the All Urban Consumers measure\nSource: Bureau Of Labor Statistics")+xlab("")+ylab("")+
+       caption = "*CORE Consumer Price Index excluding food and energy commodities\nAll Urban Consumers measures\nSource: Bureau Of Labor Statistics")+xlab("")+ylab("")+
     theme(panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank(),
         plot.title = element_text(size = 16, face="bold"),
         plot.caption = element_text(hjust = 0,size = 9),
         legend.text=element_text(size=8),
-        axis.text.x = element_text(vjust = -2))+
-   annotate("text", x = as.Date("2020-05-01"), y = 3,colour = "black", label = "COVID-19 Recession",fontface = 'italic',size = 2.3)+
+        axis.text.x = element_text(vjust = -2))+ 
+  annotate("text", x = as.Date("2020-09-01"), y = 2.6,colour = "blue", label = "Fed Inflation Target Rate",fontface = 'italic', size=2)+
+  annotate("segment", x = as.Date("2020-09-01"), xend = as.Date("2020-09-01"), y = 2.05, yend = 2.4,colour = "blue")+
+   annotate("text", x = as.Date("2020-05-01"), y = 3.4,colour = "black", label = "COVID-19 Recession",fontface = 'italic',size = 2.3)+
   annotate("text", x = as.Date("2019-05-01"), y = 3.8,colour = "black", label = "Consumer Price Index",fontface = 'plain',size = 4.1)+
 annotate("text", x = as.Date("2019-04-15"), y = 3.5,colour = "black", label = "12-month Percent Change",fontface = 'plain',size = 2.6)
+
+
 
 
 
